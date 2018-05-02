259bf3337badb1ba65f035cc83c10baa170fab1b
xiaofan <1353815633@qq.com>
2018-05-02 10:21:19 +0800

    spec-01:URL is not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('not empty', function () {
+            for (const feed of allFeeds) {
+                expect(feed.url.length).not.toBe(0);
+            }
+            
+        });
 
 
         /* TODO: Write a test that loops through each feed
